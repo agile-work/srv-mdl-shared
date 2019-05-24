6df9d208e15848fac6c11f164769dd61c9f2c1e1
Bruno Piaui <brunopiaui@gmail.com>
2019-05-24 16:51:52 -0300

    Removing user from models

--- models/job.go
+++ models/job.go
@@ -4,41 +4,37 @@ import "time"
 
 // Job defines the struct of this object
 type Job struct {
-	ID            string    `json:"id" sql:"id" pk:"true"`
-	Code          string    `json:"code" sql:"code"`
-	Name          string    `json:"name" table:"core_translations" alias:"core_translations_name" sql:"value" on:"core_translations_name.structure_id = core_jobs.id and core_translations_name.structure_field = 'name'"`
-	Description   string    `json:"description" table:"core_translations" alias:"core_translations_description" sql:"value" on:"core_translations_description.structure_id = core_jobs.id and core_translations_description.structure_field = 'description'"`
-	JobType       string    `json:"job_type" sql:"job_type"`
-	ExecTimeout   int       `json:"exec_timeout" sql:"exec_timeout"`
-	Params        []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
-	Active        bool      `json:"active" sql:"active"`
-	CreatedBy     string    `json:"created_by" sql:"created_by"`
-	CreatedByUser *User     `json:"created_by_user" table:"core_users" alias:"created_by_user" on:"created_by_user.id = core_jobs.created_by"`
-	CreatedAt     time.Time `json:"created_at" sql:"created_at"`
-	UpdatedBy     string    `json:"updated_by" sql:"updated_by"`
-	UpdatedByUser *User     `json:"updated_by_user" table:"core_users" alias:"updated_by_user" on:"updated_by_user.id = core_jobs.updated_by"`
-	UpdatedAt     time.Time `json:"updated_at" sql:"updated_at"`
+	ID          string    `json:"id" sql:"id" pk:"true"`
+	Code        string    `json:"code" sql:"code"`
+	Name        string    `json:"name" table:"core_translations" alias:"core_translations_name" sql:"value" on:"core_translations_name.structure_id = core_jobs.id and core_translations_name.structure_field = 'name'"`
+	Description string    `json:"description" table:"core_translations" alias:"core_translations_description" sql:"value" on:"core_translations_description.structure_id = core_jobs.id and core_translations_description.structure_field = 'description'"`
+	JobType     string    `json:"job_type" sql:"job_type"`
+	ExecTimeout int       `json:"exec_timeout" sql:"exec_timeout"`
+	Params      []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
+	Active      bool      `json:"active" sql:"active"`
+	CreatedBy   string    `json:"created_by" sql:"created_by"`
+	CreatedAt   time.Time `json:"created_at" sql:"created_at"`
+	UpdatedBy   string    `json:"updated_by" sql:"updated_by"`
+	UpdatedAt   time.Time `json:"updated_at" sql:"updated_at"`
 }
 
 // ViewJobInstance defines the struct of this object
 type ViewJobInstance struct {
-	ID            string    `json:"id" sql:"id" pk:"true"`
-	JobID         string    `json:"job_id" sql:"job_id"`
-	Code          string    `json:"code" sql:"code"`
-	Name          string    `json:"name" sql:"name"`
-	Description   string    `json:"description" sql:"description"`
-	JobType       string    `json:"job_type" sql:"job_type"`
-	ExecTimeout   int       `json:"exec_timeout" sql:"exec_timeout"`
-	Params        []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
-	StartAt       time.Time `json:"start_at" sql:"start_at"`
-	FinishAt      time.Time `json:"finish_at" sql:"finish_at"`
-	Status        string    `json:"status" sql:"status"`
-	CreatedBy     string    `json:"created_by" sql:"created_by"`
-	CreatedByUser *User     `json:"created_by_user" table:"core_users" alias:"created_by_user" on:"created_by_user.id = core_v_core_job_instance.created_by"`
-	CreatedAt     time.Time `json:"created_at" sql:"created_at"`
-	UpdatedBy     string    `json:"updated_by" sql:"updated_by"`
-	UpdatedByUser *User     `json:"updated_by_user" table:"core_users" alias:"updated_by_user" on:"updated_by_user.id = core_v_core_job_instance.updated_by"`
-	UpdatedAt     time.Time `json:"updated_at" sql:"updated_at"`
+	ID          string    `json:"id" sql:"id" pk:"true"`
+	JobID       string    `json:"job_id" sql:"job_id"`
+	Code        string    `json:"code" sql:"code"`
+	Name        string    `json:"name" sql:"name"`
+	Description string    `json:"description" sql:"description"`
+	JobType     string    `json:"job_type" sql:"job_type"`
+	ExecTimeout int       `json:"exec_timeout" sql:"exec_timeout"`
+	Params      []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
+	StartAt     time.Time `json:"start_at" sql:"start_at"`
+	FinishAt    time.Time `json:"finish_at" sql:"finish_at"`
+	Status      string    `json:"status" sql:"status"`
+	CreatedBy   string    `json:"created_by" sql:"created_by"`
+	CreatedAt   time.Time `json:"created_at" sql:"created_at"`
+	UpdatedBy   string    `json:"updated_by" sql:"updated_by"`
+	UpdatedAt   time.Time `json:"updated_at" sql:"updated_at"`
 }
 
 // JobTask defines the struct of this object
@@ -104,18 +100,16 @@ type ViewJobTaskInstance struct {
 
 // JobFollowers defines the struct of this object
 type JobFollowers struct {
-	ID            string    `json:"id" sql:"id" pk:"true"`
-	JobID         string    `json:"job_id" sql:"job_id" fk:"true"`
-	Name          string    `json:"name" sql:"name"`
-	LanguageCode  string    `json:"language_code" sql:"language_code"`
-	FollowerType  string    `json:"follower_type" sql:"follower_type"`
-	Active        bool      `json:"active" sql:"active"`
-	CreatedBy     string    `json:"created_by" sql:"created_by"`
-	CreatedByUser *User     `json:"created_by_user" table:"core_users" alias:"created_by_user" on:"created_by_user.id = core_v_job_followers.created_by"`
-	CreatedAt     time.Time `json:"created_at" sql:"created_at"`
-	UpdatedBy     string    `json:"updated_by" sql:"updated_by"`
-	UpdatedByUser *User     `json:"updated_by_user" table:"core_users" alias:"updated_by_user" on:"updated_by_user.id = core_v_job_followers.updated_by"`
-	UpdatedAt     time.Time `json:"updated_at" sql:"updated_at"`
+	ID           string    `json:"id" sql:"id" pk:"true"`
+	JobID        string    `json:"job_id" sql:"job_id" fk:"true"`
+	Name         string    `json:"name" sql:"name"`
+	LanguageCode string    `json:"language_code" sql:"language_code"`
+	FollowerType string    `json:"follower_type" sql:"follower_type"`
+	Active       bool      `json:"active" sql:"active"`
+	CreatedBy    string    `json:"created_by" sql:"created_by"`
+	CreatedAt    time.Time `json:"created_at" sql:"created_at"`
+	UpdatedBy    string    `json:"updated_by" sql:"updated_by"`
+	UpdatedAt    time.Time `json:"updated_at" sql:"updated_at"`
 }
 
 // ViewFollowerAvailable defines the struct of this object
@@ -144,19 +138,17 @@ type Param struct {
 
 // JobInstance defines the struct of this object
 type JobInstance struct {
-	ID            string    `json:"id" sql:"id" pk:"true"`
-	JobID         string    `json:"job_id" sql:"job_id" fk:"true"`
-	ServiceID     string    `json:"service_id" sql:"service_id" fk:"true"`
-	Code          string    `json:"code" sql:"code"`
-	ExecTimeout   int       `json:"exec_timeout" sql:"exec_timeout"`
-	Params        []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
-	Status        string    `json:"status" sql:"status"`
-	StartAt       time.Time `json:"start_at" sql:"start_at"`
-	FinishAt      time.Time `json:"finish_at" sql:"finish_at"`
-	CreatedBy     string    `json:"created_by" sql:"created_by"`
-	CreatedByUser *User     `json:"created_by_user" table:"core_users" alias:"created_by_user" on:"created_by_user.id = core_job_instances.created_by"`
-	CreatedAt     time.Time `json:"created_at" sql:"created_at"`
-	UpdatedBy     string    `json:"updated_by" sql:"updated_by"`
-	UpdatedByUser *User     `json:"updated_by_user" table:"core_users" alias:"updated_by_user" on:"updated_by_user.id = core_job_instances.updated_by"`
-	UpdatedAt     time.Time `json:"updated_at" sql:"updated_at"`
+	ID          string    `json:"id" sql:"id" pk:"true"`
+	JobID       string    `json:"job_id" sql:"job_id" fk:"true"`
+	ServiceID   string    `json:"service_id" sql:"service_id" fk:"true"`
+	Code        string    `json:"code" sql:"code"`
+	ExecTimeout int       `json:"exec_timeout" sql:"exec_timeout"`
+	Params      []Param   `json:"parameters" sql:"parameters" field:"jsonb"`
+	Status      string    `json:"status" sql:"status"`
+	StartAt     time.Time `json:"start_at" sql:"start_at"`
+	FinishAt    time.Time `json:"finish_at" sql:"finish_at"`
+	CreatedBy   string    `json:"created_by" sql:"created_by"`
+	CreatedAt   time.Time `json:"created_at" sql:"created_at"`
+	UpdatedBy   string    `json:"updated_by" sql:"updated_by"`
+	UpdatedAt   time.Time `json:"updated_at" sql:"updated_at"`
 }

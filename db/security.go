95c252db4dc5c0e3346bc2f12c5f20bd94cd1d12
Bruno Piaui <brunopiaui@gmail.com>
2019-05-27 16:55:58 -0300

    Refactoring on function GetUserAvailableFields

--- db/security.go
+++ db/security.go
@@ -1,16 +1,39 @@
 package db
 
+import (
+	"fmt"
+
+	shared "github.com/agile-work/srv-shared"
+	"github.com/agile-work/srv-shared/sql-builder/builder"
+	sql "github.com/agile-work/srv-shared/sql-builder/db"
+)
+
 // StructurePermission defines attirbutes to a structure
 type StructurePermission struct {
 	ID             string `json:"id" sql:"id" pk:"true"`
-	Code           string `json:"code" sql:"code"`
+	StructureCode  string `json:"structure_code" sql:"structure_code"`
 	StructureType  string `json:"structure_type" sql:"structure_type"`   // field, widget, section...
 	StructureClass string `json:"structure_class" sql:"structure_class"` //field: lookup_tree, text, date, number, money | widget: chart, table
 	Permission     int    `json:"permission" sql:"permission"`
 }
 
-// GetUserAvailableStructures returns a list of available structures based on user permission
+// GetUserAvailableFields returns a list of available structures based on user permission
 // passing StructureType = "" returns all types of structures
-func GetUserAvailableStructures(UserID, SchemaCode, StructureType string) []StructurePermission {
-	return nil
+func GetUserAvailableFields(userID, schemaCode, structureType string) ([]StructurePermission, error) {
+	permissions := []StructurePermission{}
+	userIDColumn := fmt.Sprintf("%s.user_id", shared.ViewCoreStructurePermissions)
+	schemaCodeColumn := fmt.Sprintf("%s.schema_code", shared.ViewCoreStructurePermissions)
+	structureTypeColumn := fmt.Sprintf("%s.structure_type", shared.ViewCoreStructurePermissions)
+	condition := builder.And(
+		builder.Equal(userIDColumn, userID),
+		builder.Equal(schemaCodeColumn, schemaCode),
+		builder.Equal(structureTypeColumn, structureType),
+	)
+
+	err := sql.LoadStruct(shared.ViewCoreStructurePermissions, &permissions, condition)
+	if err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
 }

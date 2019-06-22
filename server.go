e1ebd6113d1dcfee86d5a81edd6569528438577b
andreluzz <andreluzz@gmail.com>
2019-06-22 00:29:05 -0300

    Refactoring

--- server.go
+++ server.go
@@ -13,11 +13,11 @@ import (
 	"time"
 
 	"github.com/agile-work/srv-mdl-shared/models/translation"
+	"github.com/agile-work/srv-shared/rdb"
+	"github.com/agile-work/srv-shared/socket"
 	"github.com/agile-work/srv-shared/util"
 
 	"github.com/agile-work/srv-shared/constants"
-
-	"github.com/agile-work/srv-shared/service"
 
 	"gopkg.in/go-playground/validator.v9"
 
@@ -36,6 +36,11 @@ var (
 	dbUser     = flag.String("dbUser", "cryoadmin", "Database user")
 	dbPassword = flag.String("dbPassword", "x3FhcrWDxnxCq9p", "Database password")
 	dbName     = flag.String("dbName", "cryo", "Database name")
+	redisHost  = flag.String("redisHost", "localhost", "Redis host")
+	redisPort  = flag.Int("redisPort", 6379, "Redis port")
+	redisPass  = flag.String("redisPass", "redis123", "Redis password")
+	wsHost     = flag.String("wsHost", "localhost", "Realtime host")
+	wsPort     = flag.Int("wsPort", 8010, "Realtime port")
 )
 
 // Validate global instance of the validator
@@ -61,6 +66,8 @@ func ListenAndServe(name, port string, moduleRouter *chi.Mux) {
 		name = "undefined"
 	}
 
+	fmt.Printf("Starting Module %s...\n", name)
+
 	caCert, err := ioutil.ReadFile(*cert)
 	if err != nil {
 		panic("Invalid service certificate")
@@ -79,11 +86,15 @@ func ListenAndServe(name, port string, moduleRouter *chi.Mux) {
 		panic("Database error")
 	}
 	defer db.Close()
+	fmt.Println("Database connected")
 
-	ws, err := service.Register(name, constants.ServiceTypeModule)
-	if err != nil {
-		fmt.Printf("\nUnable to connect to realtime socket. Error: %s", err.Error())
-	}
+	rdb.Init(*redisHost, *redisPort, *redisPass)
+	go rdb.HandleReconnection(5)
+	defer rdb.Close()
+
+	socket.Init(name, constants.ServiceTypeModule, *wsHost, *wsPort)
+	go socket.HandleReconnection(5)
+	defer socket.Close()
 
 	params, err := util.GetSystemParams()
 	if err == nil {
@@ -113,14 +124,11 @@ func ListenAndServe(name, port string, moduleRouter *chi.Mux) {
 
 	go func() {
 		fmt.Printf("Service listening on %s\n", port)
-		if err := httpServer.ListenAndServeTLS(*cert, *key); err != nil {
-			fmt.Printf("listen: %s\n", err)
-		}
+		httpServer.ListenAndServeTLS(*cert, *key)
 	}()
 
 	<-stopChan
 	fmt.Println("\nShutting down Service...")
-	ws.Down()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	httpServer.Shutdown(ctx)
 	defer cancel()
